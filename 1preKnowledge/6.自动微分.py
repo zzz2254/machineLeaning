b46b565562b555c621db4f23d29a65e6af612ede
zhouyide <747310145@qq.com>
2023-07-06 16:47:26 +0800

    7.6学习日记

---
+++ 1preKnowledge/6.自动微分.py
@@ -0,0 +1,73 @@
+import torch
+
+# 需要一个地方来存储梯度。 重要的是，我们不会在每次对一个参数求导时都分配新的内存
+x = torch.arange(4.0)
+x.requires_grad_(True)
+# x = torch.arange(4.0, requires_grad=True)  # 和上面两行等价
+print(x)
+# print(x.grad)  # 默认值是None
+
+# 定义函数关于列向量x
+y = 2 * torch.dot(x, x)
+# print(y)
+
+# 调用反向传播函数来自动计算y关于x每个分量的梯度
+y.backward()
+# print(x.grad)
+
+# 验证这个梯度是否计算正确
+# print(x.grad == 4 * x)
+
+# 在默认情况下，PyTorch会累积梯度，我们需要清除之前的值
+x.grad.zero_()
+y = x.sum()
+# print(y)
+y.backward()
+# print(x.grad)
+
+# 非标量变量的反向传播
+x.grad.zero_()
+y = x * x
+# print(y)
+y.sum().backward()
+# print(x.grad)
+
+"""
+分离计算
+"""
+# 反向传播函数计算z=u*x关于x的偏导数，同时将u作为常数处理
+x.grad.zero_()
+y = x * x
+u = y.detach()
+z = u * x
+z.sum().backward()
+# print(x.grad)
+# print(u)
+# print(z)
+# print(x.grad == u)
+
+# 求 y = x*x 的导数
+x.grad.zero_()
+y.sum().backward()
+# print(x.grad == 2 * x)
+
+"""
+Python控制流的梯度计算
+"""
+def f(a):
+    b = a * 2
+    while b.norm() < 1000:
+        b = b * 2
+    if b.sum() > 0:
+        c = b
+    else:
+        c = 100 * b
+    return c
+
+
+a = torch.randn(size=(), requires_grad=True)
+print(a)
+d = f(a)
+d.backward()
+
+print(a.grad == d/a)
